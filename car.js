b39e9cdfcda57d0b1d75651f9d4ec5a4365b05e9
Chemeka Pierce <cdpierce81@gmail.com>
2018-05-29 11:46:41 -0400

    made changes

--- car.js
+++ car.js
@@ -14,3 +14,15 @@ function carousel() {
 	x[myIndex - 1].style.display = "block";
 	setTimeout(carousel, 2000); //Change image every 2 seconds
 }
+
+function openNav() {
+				document.getElementById("mySidemenu").style.width = "150px";
+				document.getElementById("main").style.marginLeft = "150px";
+				document.body.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
+				}
+			
+			function closeNav() {
+				document.getElementById("mySidemenu").style.width = "0";
+				document.getElementById("main").style.marginLeft = "0";
+				document.body.style.backgroundColor = "LightGrey";
+				}
