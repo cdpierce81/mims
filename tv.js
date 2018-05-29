9049fe3e0a17162c10cd602015509b5e0c23e353
Chemeka Pierce <cdpierce81@gmail.com>
2018-05-29 15:22:12 -0400

    changes

--- tv.js
+++ tv.js
@@ -16,13 +16,13 @@ function carousel() {
 }
 
 function openNav() {
-				document.getElementById("mySidemenu").style.width = "150px";
-				document.getElementById("main").style.marginLeft = "150px";
-				document.body.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
-				}
-			
-			function closeNav() {
-				document.getElementById("mySidemenu").style.width = "0";
-				document.getElementById("main").style.marginLeft = "0";
-				document.body.style.backgroundColor = "LightGrey";
-				}
+	document.getElementById("mySidemenu").style.width = "150px";
+	document.getElementById("main").style.marginLeft = "150px";
+	document.body.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
+}
+
+function closeNav() {
+	document.getElementById("mySidemenu").style.width = "0";
+	document.getElementById("main").style.marginLeft = "0";
+	document.body.style.backgroundColor = "LightGrey";
+}
